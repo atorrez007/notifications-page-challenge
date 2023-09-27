def5456e8a337b468bc4579bf8e1fab7cc4c2091
funkee <xxtrm98@protonmail.com>
2023-09-27 10:51:37 -0500

    testing sass styles import to component and deployment

---
+++ src/Components/TestComponent.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Styles/TestComponentStyles.css";
+
+const TestComponent = () => {
+  return (
+    <div className="test-styles">
+      <h1>Test Component</h1>
+    </div>
+  );
+};
+
+export default TestComponent;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import "../Styles/Mainstyles.css";
+import TestComponent from "@/Components/TestComponent";
+
 export default function Home() {
   return (
     <div>
       <h1>Notifications Challenge</h1>
+      <TestComponent />
     </div>
   );
 }
