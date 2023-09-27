3bdb16fa7c06adbac267ee60a619f68558378b5b
funkee <xxtrm98@protonmail.com>
2023-09-27 17:35:53 -0500

    removed .next folder and reinstalled

---
+++ src/components/Notifications.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../Styles/NotificationStyles.css";
+
+const Notifications = () => {
+  return (
+    <div className="notifications-panel">
+      <div>
+        <h2>Notification 1</h2>
+      </div>
+      <div>
+        <h2>Notification 2</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Notifications;
