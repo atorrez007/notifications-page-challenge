464b3b2a20887df52cb812fd24737d821b1b5633
funkee <xxtrm98@protonmail.com>
2023-09-29 10:52:26 -0500

    styled notifications feedback

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -4,11 +4,16 @@ import "../Styles/NotificationStyles.css";
 const Notifications = () => {
   return (
     <div className="notifications-panel">
-      <div>
-        <p>Notification 1</p>
-      </div>
-      <div>
-        <p>Notification 2</p>
+      <div className="notification-feedback">
+        <div className="notifications">
+          <div className="text-and-count">
+            <h3>Notifications</h3>
+            <span className="inbox-counter">3</span>
+          </div>
+        </div>
+        <div className="read-all">
+          <p>Mark all as read</p>
+        </div>
       </div>
     </div>
   );
